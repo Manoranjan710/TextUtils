a25f6af7a077e19e7cfde964f4d53977a8f61c52
Manoranjan710 <manoranjan710@gmail.com>
2023-04-07 15:14:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from "./components/Navbar.js";
+
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils2" aboutText="About TextUtils" />
+    </>
+  );
+}
+
+export default App;
